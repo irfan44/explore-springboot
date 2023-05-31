12cc67f94bfc1e3122a8bd0fe69ac54af3bf3252
U549071 <irfan_muhajir@intra.bca>
2023-05-31 14:58:57 +0700

    init: init spring app

---
+++ src/main/java/dev/irfannm/exploreSpring/ExploreSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.irfannm.exploreSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringApplication.class, args);
+	}
+
+}
